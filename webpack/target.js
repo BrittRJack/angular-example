1dbbd138eb9a504454bb2d8b53944ede407c1657
George Kephart <Gkephart@cnm.edu>
2018-08-17 11:24:41 -0600

    added the target.js template for reference.

---
+++ webpack/target.js
@@ -0,0 +1,4 @@
+module.exports = function() {
+	//path to your deployment API directory. You will need to customize this file to match your username and project-name
+	return basepath = "https://bootcamp-coders.cnm.edu/~username/project-name/public_html/"; //Your URL here
+};
